9f98f66067b5632163d89896d052f4a48ae3ce7b
Marina <nmo1@ya.ru>
2024-12-15 20:39:38 +0300

    2. Tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -20,13 +20,41 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const result = postsService.findMany();
+
+      result.forEach((it, key) => expect(it).toHaveProperty('text', posts[key].text));
+      expect(result).toHaveLength(posts.length);
+
+      expect(result).not.toHaveLength(0);
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const skip = 1;
+      const limit = 2;
+
+      const result = postsService.findMany({ skip, limit });
+      const postsSliced = posts.slice(skip, skip + limit);
+
+      result.forEach((it, key) => expect(it).toHaveProperty('text', postsSliced[key].text))
+      expect(result).toHaveLength(limit);
     });
 
-    // реализуйте недостающие тест-кейсы
+    it('should return correct posts for only skip option', () => {
+      const skip = 1;
+
+      const result = postsService.findMany({ skip });
+      const expectedResult = posts.slice(skip);
+
+      expect(result[0]).toHaveProperty('text', expectedResult[0].text);
+      expect(result).toHaveLength(expectedResult.length);
+    });
+
+    it('should return correct posts for only limit option', () => {
+      const limit = 2;
+
+      const result = postsService.findMany({ limit });
+
+      expect(result).toHaveLength(limit);
+    });
   });
 });
